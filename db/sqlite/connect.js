d4aac82efbacaf6598d93d16e836a2710066cb0a
Karl Stolley <karl.stolley@gmail.com>
2020-03-04 15:39:02 -0600

    Establish connect.js as a module

--- db/sqlite/connect.js
+++ db/sqlite/connect.js
@@ -1,16 +1,15 @@
 const sqlite = require('sqlite3').verbose();
 
-const db = new sqlite.Database('development.sqlite3',(err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log('Connected to the database');
-  }
-});
+function connect(database = 'development.sqlite3') {
+  return new sqlite.Database(database,(err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`Connected to the database ${database}`);
+    }
+  });
+}
 
-db.close((err) => {
-  if (err) {
-    console.err(err.message);
-  }
-  console.log('Database connection closed');
-});
+module.exports = {
+  connect
+}

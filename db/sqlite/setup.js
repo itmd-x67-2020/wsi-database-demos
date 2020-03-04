2281f11a4311e9f635f74550dffff2647caff8a4
Karl Stolley <karl.stolley@gmail.com>
2020-03-04 16:20:39 -0600

    Add seed data to the database

--- db/sqlite/setup.js
+++ db/sqlite/setup.js
@@ -1,5 +1,11 @@
 const db = require('./connect.js').connect();
 
+const seed_data = [
+  { date: '2020-02-29', max: 42.4, min: 17.1 , avg: 29.4 },
+  { date: '2020-03-01', max: 52.1, min: 22.1 , avg: 26.4 },
+  { date: '2020-03-02', max: 47.9, min: 33.2 , avg: 30.4 }
+];
+
 
 db.serialize(() => {
   let drop_table = `DROP TABLE IF EXISTS daily_records`;
@@ -20,6 +26,24 @@ db.serialize(() => {
       console.error(err.message);
     }
   });
+
+  let stmt = db.prepare(`INSERT INTO daily_records(obs_date,max_temp,min_temp,avg_temp) VALUES (?,?,?,?)`);
+
+  for (let d of seed_data) {
+    console.log("Values:",d.date,d.max,d.min,d.avg);
+    stmt.run(d.date,d.max,d.min,d.avg);
+  }
+
+  stmt.finalize();
+
+  db.each("SELECT max_temp FROM daily_records", (err,row) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log("Max temp:", row.max_temp);
+    }
+  });
+
 });
 
 db.close((err) => {

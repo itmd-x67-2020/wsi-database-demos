2061b7e023325dba1deb2352203ae1123b79e2cf
Karl Stolley <karl.stolley@gmail.com>
2020-03-25 17:43:51 -0500

    Properly handle UTF8-encoded text file

--- fs/diagnostic.js
+++ fs/diagnostic.js
@@ -2,7 +2,10 @@ const fsp = require('fs').promises;
 
 (async function() {
   try {
-    const records = await fsp.readFile('./wx_records.json');
+    // Provide 'utf8' encoding argument to readFile; see
+    // https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+    const records = await fsp.readFile('./wx_records.json','utf8');
+    // Log the raw records file contents as returned
     console.log(records);
   } catch(err) {
     console.log(err.message);

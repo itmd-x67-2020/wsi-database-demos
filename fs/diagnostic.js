293a1821276986799afbed0847fc06dee2272462
Karl Stolley <karl.stolley@gmail.com>
2020-03-25 17:44:05 -0500

    Parse and return a value from records file

--- fs/diagnostic.js
+++ fs/diagnostic.js
@@ -5,8 +5,12 @@ const fsp = require('fs').promises;
     // Provide 'utf8' encoding argument to readFile; see
     // https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
     const records = await fsp.readFile('./wx_records.json','utf8');
+    // Parse the records file
+    const records_obj = JSON.parse(records);
     // Log the raw records file contents as returned
     console.log(records);
+    // Log, as an exaple, the max_temp record's value
+    console.log("Max Temp:", records_obj.records.max_temp.value)
   } catch(err) {
     console.log(err.message);
   }
